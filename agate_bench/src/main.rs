2a5fe8b47150315d3f4d855ef7d0453f81a9b31f
Jason Goldberger <jasongoldberger@gmail.com>
2023-03-18 14:07:19 -0500

    fix enable-rocksdb modules

--- agate_bench/src/main.rs
+++ agate_bench/src/main.rs
@@ -1,48 +1,53 @@
-use std::{path::PathBuf, sync::Arc, time::Instant};
+#[cfg(feature = "enable-rocksdb")]
+mod bench {
 
-use agatedb::AgateOptions;
-use clap::clap_app;
-use indicatif::{ProgressBar, ProgressStyle};
+    use std::{path::PathBuf, sync::Arc, time::Instant};
 
-#[path = "../../benches/common.rs"]
-mod common;
+    use agatedb::AgateOptions;
+    use clap::clap_app;
+    use indicatif::{ProgressBar, ProgressStyle};
 
-use common::{
-    agate_iterate, agate_populate, agate_randread, rocks_iterate, rocks_populate, rocks_randread,
-};
+    #[path = "../../benches/common.rs"]
+    mod common;
 
-pub struct Rate {
-    pub data: std::sync::Arc<std::sync::atomic::AtomicU64>,
-    lst_data: u64,
-}
+    use common::{
+        agate_iterate, agate_populate, agate_randread, rocks_iterate, rocks_populate,
+        rocks_randread,
+    };
 
-impl Rate {
-    pub fn new() -> Self {
-        Self {
-            data: std::sync::Arc::new(std::sync::atomic::AtomicU64::new(0)),
-            lst_data: 0,
+    pub struct Rate {
+        pub data: std::sync::Arc<std::sync::atomic::AtomicU64>,
+        lst_data: u64,
+    }
+
+    impl Rate {
+        pub fn new() -> Self {
+            Self {
+                data: std::sync::Arc::new(std::sync::atomic::AtomicU64::new(0)),
+                lst_data: 0,
+            }
+        }
+
+        pub fn now(&self) -> u64 {
+            self.data.load(std::sync::atomic::Ordering::Relaxed)
+        }
+
+        pub fn rate(&mut self) -> u64 {
+            let now = self.now();
+            let delta = now - self.lst_data;
+            self.lst_data = now;
+            delta
         }
     }
 
-    pub fn now(&self) -> u64 {
-        self.data.load(std::sync::atomic::Ordering::Relaxed)
+    impl Default for Rate {
+        fn default() -> Self {
+            Self::new()
+        }
     }
 
-    pub fn rate(&mut self) -> u64 {
-        let now = self.now();
-        let delta = now - self.lst_data;
-        self.lst_data = now;
-        delta
-    }
-}
-
-impl Default for Rate {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-fn main() {
-    let matches = clap_app!(agate_bench =>
+    pub fn run() {
+        let matches = clap_app!(agate_bench =>
         (version: "1.0")
         (author: "TiKV authors")
         (about: "Benchmark for AgateDB")
@@ -101,185 +106,201 @@ fn main() {
     )
     .get_matches();
 
-    let directory = PathBuf::from(matches.value_of("directory").unwrap());
-    let threads: u64 = matches.value_of("threads").unwrap().parse().unwrap();
+        let directory = PathBuf::from(matches.value_of("directory").unwrap());
+        let threads: u64 = matches.value_of("threads").unwrap().parse().unwrap();
 
-    let mut agate_opts = AgateOptions {
-        dir: directory.clone(),
-        value_dir: directory.clone(),
-        sync_writes: true,
-        managed_txns: true,
-        ..Default::default()
-    };
+        let mut agate_opts = AgateOptions {
+            dir: directory.clone(),
+            value_dir: directory.clone(),
+            sync_writes: true,
+            managed_txns: true,
+            ..Default::default()
+        };
 
-    let mut rocks_opts = rocksdb::Options::default();
-    rocks_opts.create_if_missing(true);
-    rocks_opts.set_compression_type(rocksdb::DBCompressionType::None);
+        let mut rocks_opts = rocksdb::Options::default();
+        rocks_opts.create_if_missing(true);
+        rocks_opts.set_compression_type(rocksdb::DBCompressionType::None);
 
-    match matches.subcommand() {
-        ("populate", Some(sub_matches)) => {
-            let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
-            let batch_size: u64 = sub_matches.value_of("batch_size").unwrap().parse().unwrap();
-            let value_size: usize = sub_matches.value_of("value_size").unwrap().parse().unwrap();
-            let seq: bool = sub_matches.value_of("seq").unwrap().parse().unwrap();
+        match matches.subcommand() {
+            ("populate", Some(sub_matches)) => {
+                let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
+                let batch_size: u64 = sub_matches.value_of("batch_size").unwrap().parse().unwrap();
+                let value_size: usize =
+                    sub_matches.value_of("value_size").unwrap().parse().unwrap();
+                let seq: bool = sub_matches.value_of("seq").unwrap().parse().unwrap();
 
-            let agate = Arc::new(agate_opts.open().unwrap());
-            let chunk_size = key_nums / threads;
+                let agate = Arc::new(agate_opts.open().unwrap());
+                let chunk_size = key_nums / threads;
 
-            let begin = Instant::now();
+                let begin = Instant::now();
 
-            agate_populate(agate, key_nums, chunk_size, batch_size, value_size, seq);
+                agate_populate(agate, key_nums, chunk_size, batch_size, value_size, seq);
 
-            let cost = begin.elapsed();
+                let cost = begin.elapsed();
 
-            println!("populate {} keys in {} ms", key_nums, cost.as_millis());
-        }
-        ("randread", Some(sub_matches)) => {
-            let times: u64 = sub_matches.value_of("times").unwrap().parse().unwrap();
-            let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
-            let value_size: usize = sub_matches.value_of("value_size").unwrap().parse().unwrap();
+                println!("populate {} keys in {} ms", key_nums, cost.as_millis());
+            }
+            ("randread", Some(sub_matches)) => {
+                let times: u64 = sub_matches.value_of("times").unwrap().parse().unwrap();
+                let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
+                let value_size: usize =
+                    sub_matches.value_of("value_size").unwrap().parse().unwrap();
 
-            let agate = Arc::new(agate_opts.open().unwrap());
-            let chunk_size = key_nums / threads;
+                let agate = Arc::new(agate_opts.open().unwrap());
+                let chunk_size = key_nums / threads;
 
-            let pb = ProgressBar::new(key_nums * times);
-            pb.set_style(ProgressStyle::default_bar()
+                let pb = ProgressBar::new(key_nums * times);
+                pb.set_style(ProgressStyle::default_bar()
             .template(
                 "{prefix:.bold.dim} [{elapsed_precise}] [{bar:40}] [{per_sec}] ({pos}/{len}) {msg}",
             )
             .progress_chars("=> "));
 
-            let begin = Instant::now();
+                let begin = Instant::now();
 
-            for _ in 0..times {
-                agate_randread(agate.clone(), key_nums, chunk_size, value_size);
-                pb.inc(key_nums);
+                for _ in 0..times {
+                    agate_randread(agate.clone(), key_nums, chunk_size, value_size);
+                    pb.inc(key_nums);
+                }
+
+                pb.finish_with_message("done");
+
+                let cost = begin.elapsed();
+                println!(
+                    "randread {} keys {} times in {} ms",
+                    key_nums,
+                    times,
+                    cost.as_millis()
+                );
             }
+            ("iterate", Some(sub_matches)) => {
+                let times: u64 = sub_matches.value_of("times").unwrap().parse().unwrap();
+                let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
+                let value_size: usize =
+                    sub_matches.value_of("value_size").unwrap().parse().unwrap();
 
-            pb.finish_with_message("done");
+                let agate = Arc::new(agate_opts.open().unwrap());
+                let chunk_size = key_nums / threads;
 
-            let cost = begin.elapsed();
-            println!(
-                "randread {} keys {} times in {} ms",
-                key_nums,
-                times,
-                cost.as_millis()
-            );
-        }
-        ("iterate", Some(sub_matches)) => {
-            let times: u64 = sub_matches.value_of("times").unwrap().parse().unwrap();
-            let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
-            let value_size: usize = sub_matches.value_of("value_size").unwrap().parse().unwrap();
-
-            let agate = Arc::new(agate_opts.open().unwrap());
-            let chunk_size = key_nums / threads;
-
-            let pb = ProgressBar::new(key_nums * times);
-            pb.set_style(ProgressStyle::default_bar()
+                let pb = ProgressBar::new(key_nums * times);
+                pb.set_style(ProgressStyle::default_bar()
             .template(
                 "{prefix:.bold.dim} [{elapsed_precise}] [{bar:40}] [{per_sec}] ({pos}/{len}) {msg}",
             )
             .progress_chars("=> "));
 
-            let begin = Instant::now();
+                let begin = Instant::now();
 
-            for _ in 0..times {
-                agate_iterate(agate.clone(), key_nums, chunk_size, value_size);
-                pb.inc(key_nums);
+                for _ in 0..times {
+                    agate_iterate(agate.clone(), key_nums, chunk_size, value_size);
+                    pb.inc(key_nums);
+                }
+
+                pb.finish_with_message("done");
+
+                let cost = begin.elapsed();
+                println!(
+                    "iterate {} keys {} times in {} ms",
+                    key_nums,
+                    times,
+                    cost.as_millis()
+                );
             }
+            ("rocks_populate", Some(sub_matches)) => {
+                let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
+                let batch_size: u64 = sub_matches.value_of("batch_size").unwrap().parse().unwrap();
+                let value_size: usize =
+                    sub_matches.value_of("value_size").unwrap().parse().unwrap();
+                let seq: bool = sub_matches.value_of("seq").unwrap().parse().unwrap();
 
-            pb.finish_with_message("done");
+                let db = Arc::new(rocksdb::DB::open(&rocks_opts, &directory).unwrap());
+                let chunk_size = key_nums / threads;
 
-            let cost = begin.elapsed();
-            println!(
-                "iterate {} keys {} times in {} ms",
-                key_nums,
-                times,
-                cost.as_millis()
-            );
-        }
-        ("rocks_populate", Some(sub_matches)) => {
-            let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
-            let batch_size: u64 = sub_matches.value_of("batch_size").unwrap().parse().unwrap();
-            let value_size: usize = sub_matches.value_of("value_size").unwrap().parse().unwrap();
-            let seq: bool = sub_matches.value_of("seq").unwrap().parse().unwrap();
+                let begin = Instant::now();
 
-            let db = Arc::new(rocksdb::DB::open(&rocks_opts, &directory).unwrap());
-            let chunk_size = key_nums / threads;
+                rocks_populate(db, key_nums, chunk_size, batch_size, value_size, seq);
 
-            let begin = Instant::now();
+                let cost = begin.elapsed();
 
-            rocks_populate(db, key_nums, chunk_size, batch_size, value_size, seq);
+                println!("populate {} keys in {} ms", key_nums, cost.as_millis());
+            }
+            ("rocks_randread", Some(sub_matches)) => {
+                let times: u64 = sub_matches.value_of("times").unwrap().parse().unwrap();
+                let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
+                let value_size: usize =
+                    sub_matches.value_of("value_size").unwrap().parse().unwrap();
 
-            let cost = begin.elapsed();
+                let db = Arc::new(rocksdb::DB::open(&rocks_opts, &directory).unwrap());
+                let chunk_size = key_nums / threads;
 
-            println!("populate {} keys in {} ms", key_nums, cost.as_millis());
-        }
-        ("rocks_randread", Some(sub_matches)) => {
-            let times: u64 = sub_matches.value_of("times").unwrap().parse().unwrap();
-            let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
-            let value_size: usize = sub_matches.value_of("value_size").unwrap().parse().unwrap();
-
-            let db = Arc::new(rocksdb::DB::open(&rocks_opts, &directory).unwrap());
-            let chunk_size = key_nums / threads;
-
-            let pb = ProgressBar::new(key_nums * times);
-            pb.set_style(ProgressStyle::default_bar()
+                let pb = ProgressBar::new(key_nums * times);
+                pb.set_style(ProgressStyle::default_bar()
             .template(
                 "{prefix:.bold.dim} [{elapsed_precise}] [{bar:40}] [{per_sec}] ({pos}/{len}) {msg}",
             )
             .progress_chars("=> "));
 
-            let begin = Instant::now();
+                let begin = Instant::now();
 
-            for _ in 0..times {
-                rocks_randread(db.clone(), key_nums, chunk_size, value_size);
-                pb.inc(key_nums);
+                for _ in 0..times {
+                    rocks_randread(db.clone(), key_nums, chunk_size, value_size);
+                    pb.inc(key_nums);
+                }
+
+                pb.finish_with_message("done");
+
+                let cost = begin.elapsed();
+                println!(
+                    "randread {} keys {} times in {} ms",
+                    key_nums,
+                    times,
+                    cost.as_millis()
+                );
             }
+            ("rocks_iterate", Some(sub_matches)) => {
+                let times: u64 = sub_matches.value_of("times").unwrap().parse().unwrap();
+                let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
+                let value_size: usize =
+                    sub_matches.value_of("value_size").unwrap().parse().unwrap();
 
-            pb.finish_with_message("done");
+                let db = Arc::new(rocksdb::DB::open(&rocks_opts, &directory).unwrap());
+                let chunk_size = key_nums / threads;
 
-            let cost = begin.elapsed();
-            println!(
-                "randread {} keys {} times in {} ms",
-                key_nums,
-                times,
-                cost.as_millis()
-            );
-        }
-        ("rocks_iterate", Some(sub_matches)) => {
-            let times: u64 = sub_matches.value_of("times").unwrap().parse().unwrap();
-            let key_nums: u64 = sub_matches.value_of("key_nums").unwrap().parse().unwrap();
-            let value_size: usize = sub_matches.value_of("value_size").unwrap().parse().unwrap();
-
-            let db = Arc::new(rocksdb::DB::open(&rocks_opts, &directory).unwrap());
-            let chunk_size = key_nums / threads;
-
-            let pb = ProgressBar::new(key_nums * times);
-            pb.set_style(ProgressStyle::default_bar()
+                let pb = ProgressBar::new(key_nums * times);
+                pb.set_style(ProgressStyle::default_bar()
             .template(
                 "{prefix:.bold.dim} [{elapsed_precise}] [{bar:40}] [{per_sec}] ({pos}/{len}) {msg}",
             )
             .progress_chars("=> "));
 
-            let begin = Instant::now();
+                let begin = Instant::now();
 
-            for _ in 0..times {
-                rocks_iterate(db.clone(), key_nums, chunk_size, value_size);
-                pb.inc(key_nums);
+                for _ in 0..times {
+                    rocks_iterate(db.clone(), key_nums, chunk_size, value_size);
+                    pb.inc(key_nums);
+                }
+
+                pb.finish_with_message("done");
+
+                let cost = begin.elapsed();
+                println!(
+                    "iterate {} keys {} times in {} ms",
+                    key_nums,
+                    times,
+                    cost.as_millis()
+                );
             }
 
-            pb.finish_with_message("done");
-
-            let cost = begin.elapsed();
-            println!(
-                "iterate {} keys {} times in {} ms",
-                key_nums,
-                times,
-                cost.as_millis()
-            );
+            _ => panic!("unsupported command"),
         }
-
-        _ => panic!("unsupported command"),
     }
+}
+
+#[cfg(not(feature = "enable-rocksdb"))]
+mod bench {
+    pub fn run() {}
+}
+
+fn main() {
+    bench::run()
 }
